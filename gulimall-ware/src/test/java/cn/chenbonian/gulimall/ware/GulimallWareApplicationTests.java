5f360aaf0a007f05bab3a635fb85126a433e015a
chenbainian <284049379@qq.com>
2020-08-10 23:51:00 +0800

    项目初始结构创建

---
+++ gulimall-ware/src/test/java/cn/chenbonian/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,11 @@
+package cn.chenbonian.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
